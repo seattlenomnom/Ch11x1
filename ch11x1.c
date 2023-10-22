a81574be5ed8acc52f54b103dd4692ac8fb82c7d
seattlenomnom <mark.crapser@gmail.com>
2023-10-21 19:44:33 -0700

    changed things around

--- ch11x1.c
+++ ch11x1.c
@@ -1,28 +1,23 @@
 /* Source file for ch11x1
  *
+ * Bitwise AND
+ *  use on binary numbers, then octal numbers, finally hex numbers. First
+ *  problem, no easy way to express binary using printf.
  *
- *
+ *  Develop a function that takes two  character strings of 1's and 0's,
+ *  and outputs the bitwise AND as a character string*
  *
 */
 
-
-
-
-
-
+#include <stdio.h>
 
 int main(int argc, char *argv[]){
 
-
-
-
-
-
-
-
-
-
-
+    char result[17] = {"Missing function"};
+    printf("Bitwise AND\n");
+    printf("%s\n", argv[1]);
+    printf("%s\n", argv[2]);
+    printf("%s\n", result);
 
 
 
